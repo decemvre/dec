f9824a933f2251d9283801ba1f820813da3e3e72
Mircea Silviu Itu <decemvre@gmail.com>
2015-09-20 09:43:12 +0300

    commit

--- app/dec/views/layouts/main.php
+++ app/dec/views/layouts/main.php
@@ -11,7 +11,7 @@ use dec\components\Assets;
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Simple ADR Web Application</title>
+  <title>Simple MVC Web Application</title>
 
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
